bc9d2868ad42dc14dbccf17902039b92bb1518eb
jamornP <100953989+jamornP@users.noreply.github.com>
2023-08-08 09:37:21 +0700

    up

---
+++ components/link.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="/template/theme/css/bootstrap-theme.css">
+<link rel="stylesheet" href="/template/theme/css/color.css">
---
+++ pages/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template</title>
+    <?php require $_SERVER['DOCUMENT_ROOT'] . "/template/components/link.php"; ?>
+</head>
+<body class="font-kanit">
+    <?php require $_SERVER['DOCUMENT_ROOT'] . "/template/components/navbar.php"; ?>
+
+
+
+
+
+
+    
+    <?php require $_SERVER['DOCUMENT_ROOT'] . "/template/components/script.php"; ?>
+</body>
+</html>
